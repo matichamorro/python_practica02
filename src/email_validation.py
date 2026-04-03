837d95618cc629860dd9da8a0b3ffc50829bf510
matichamorro <matias.chamorro@hotmail.com>
2026-04-03 19:43:54 -0300

    Update: email_verification.py

--- src/email_validation.py
+++ src/email_validation.py
@@ -2,7 +2,7 @@
 def validate_email():
     email = input("Ingrese un email: ")
     validating = "El email no es válido."
-    if email.count('@') == 1:
+    if email.count('@') == 1 and not email.startswith(('@','.')) and not email.endswith(('@','.')):
         email = email.split('@')
         if len(email[0]) >= 1 and email[1].count('.') >= 1:
                 email_ending = email[1].split('.')
